e6fc1470014688d74fbf9e6da5b912e4dc1f5d10
Anik Sur <62329547+Anik-Sur@users.noreply.github.com>
2022-04-12 19:02:54 +0530

    added fib.c

---
+++ DP/fib.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+unsigned long long int fib (int n) {
+
+    static map <int, unsigned long long int> memo;
+    if (n<=2){
+        return 1;
+    }
+    if (memo.count(n) > 0) {
+        return memo[n];
+    }
+    else
+    {
+        memo[n] = fib(n-1) + fib(n-2);
+        return memo[n];
+    }
+}
+
+int main(){
+    int n=0;
+    static map <int, unsigned long long int> memo;
+    cout << "enter place of fibonnachi" << endl;
+    cin >> n;
+    int ans = fib(n);
+    cout << ans;
+}
